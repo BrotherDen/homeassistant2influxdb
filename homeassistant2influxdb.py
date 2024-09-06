56253523cfb3e583eddb46a6a6a02f69a2e8ae83
BrotherDen <hi-pro@inbox.ru>
2024-09-06 20:26:52 +0500

    added VictoriaMetrics support

--- homeassistant2influxdb.py
+++ homeassistant2influxdb.py
@@ -1,16 +1,16 @@
-#!.venv/bin/python3
 # -*- coding: utf-8 -*-
 
 import argparse
 from datetime import datetime
 from homeassistant.core import Event, State
-from homeassistant.components.influxdb import get_influx_connection, _generate_event_to_json, INFLUX_SCHEMA
+from homeassistant.components.influxdb import get_influx_connection, INFLUX_SCHEMA
 from homeassistant.exceptions import InvalidEntityFormatError
 import json
 import sys
 from tqdm import tqdm
 import voluptuous as vol
 import yaml
+import requests
 
 sys.path.append("home-assistant-core")
 
@@ -28,14 +28,8 @@ except:
 
 
 def rename_entity_id(old_name: str) -> str:
-    """
-    Given an entity_id, rename it to something else. Helpful if ids changed
-    during the course of history and you want to quickly merge the data. Beware
-    that no further adjustment is done, also no checks whether the referred
-    sensors are even compatible.
-    """
     rename_table = {
-            "sensor.old_entity_name": "sensor.new_entity_name",
+        "sensor.old_entity_name": "sensor.new_entity_name",
     }
 
     if old_name in rename_table:
@@ -45,13 +39,8 @@ def rename_entity_id(old_name: str) -> str:
 
 
 def rename_friendly_name(attributes: dict) -> dict:
-    """
-    Given the attributes to be stored, replace the friendly name. Helpful
-    if names changed during the course of history and you want to quickly
-    correct the naming.
-    """
     rename_table = {
-            "Old Sensor Name": "New Sensor Name",
+        "Old Sensor Name": "New Sensor Name",
     }
 
     if "friendly_name" in attributes and attributes["friendly_name"] in rename_table:
@@ -61,9 +50,6 @@ def rename_friendly_name(attributes: dict) -> dict:
 
 
 def create_statistics_attributes(mean: float, min: float, max: float, attributes: dict) -> dict:
-    """
-    Add min, max, mean to attributes. Run friendly name converter
-    """
     attributes = rename_friendly_name(attributes)
     attributes['mean'] = mean
     attributes['min'] = min
@@ -71,15 +57,60 @@ def create_statistics_attributes(mean: float, min: float, max: float, attributes
     return attributes
 
 
-def main():
-    """
-    Connect to both databases and migrate data
-    """
+def convert_to_line_protocol(event):
+    """Converts Home Assistant event to InfluxDB Line Protocol format"""
+    state = event.data.get("new_state")
+    measurement = state.entity_id
+    tags = []
+    fields = []
+    timestamp = int(event.time_fired.timestamp() * 1e9)
 
+    def process_dict(prefix, dictionary):
+        """Helper function to process nested dictionaries."""
+        for key, value in dictionary.items():
+            if isinstance(value, str):
+                value = value.replace(" ", "_")
+            elif isinstance(value, dict):
+                process_dict(key, value)
+            else:
+                fields.append(f"{key}={value}")
+
+    for key, value in state.attributes.items():
+        # Заменяем пробелы на подчеркивания в именах тегов
+        if key not in ('device_class', 'state_class', 'icon'):
+            key = key.replace(" ", "_")
+            '''
+            if isinstance(value, dict):
+                process_dict(key, value)
+            else:
+                if isinstance(value, str):                
+                    value = value.replace(" ", "_")
+                    '''
+            #if isinstance(value, str) and " " in value:
+            #    value = f'""{value}""'
+            if isinstance(value, str):
+                value = value.replace(" ", "_")
+            tags.append(f"{key}={value}")
+
+    # Добавляем основное значение состояния в поля
+    fields.append(f"value={state.state}")
+
+    # Собираем финальную строку
+    line = f"{measurement},{','.join(tags)} {','.join(fields)} {timestamp}"
+
+    return line
+
+
+def main():
+
+    '''
     parser = argparse.ArgumentParser()
     parser.add_argument('--type', '-t',
                         dest='type', action='store', required=True,
                         help='Database type: MySQL, MariaDB or SQLite')
+    parser.add_argument('--db', '-b',
+                        dest='db', action='store', required=True, choices=['influx', 'victoria'],
+                        help='Target database: influx or victoria')
     parser.add_argument('--user', '-u',
                         dest='user', action='store', required=False,
                         help='MySQL/MariaDB username')
@@ -102,18 +133,34 @@ def main():
                              'Home Assistant keeps 10 days of states by default and keeps statistics forever for some entities')
     parser.add_argument('--dry-run', '-y',
                         dest='dry', action='store_true', required=False,
-                        help='do all work except writing to InfluxDB')
+                        help='do all work except writing to InfluxDB or VictoriaMetrics')
+    parser.add_argument('--victoria-url', '-v',
+                        dest='victoria_url', action='store', required=True,
+                        help='VictoriaMetrics write URL, e.g., http://localhost:8428/write')
 
     args = parser.parse_args()
 
-    if (args.dry):
-        print("option --dry-run was given, nothing will be writen on InfluxDB")
+    '''
+    args = argparse.Namespace(
+        type = 'MariaDB',
+        db = 'victoria',
+        user = 'hass',
+        password = 'hass',
+        table = 'states',
+        host = '127.0.0.1',
+        database = 'homeassistant',
+        row_count = 0,
+        dry = None,
+        victoria_url = 'http://127.0.0.1:8428'
+    )
 
-    # load InfluxDB configuration file (the one from Home Assistant) (without using !secrets)
+
+    if args.dry:
+        print("option --dry-run was given, nothing will be written to InfluxDB or VictoriaMetrics")
+
     with open("influxdb.yaml") as config_file:
         influx_config = yaml.load(config_file, Loader=yaml.FullLoader)
 
-    # validate and extend config
     schema = vol.Schema(INFLUX_SCHEMA, extra=vol.ALLOW_EXTRA)
     influx_config = schema(influx_config)
 
@@ -121,12 +168,12 @@ def main():
         print('oopsie woopsie we made a fucky wucky, we need to add our influx credentials to influxdb.yaml')
         exit()
 
-    # establish connection to InfluxDB
-    influx = get_influx_connection(influx_config, test_write=True, test_read=True)
-    converter = _generate_event_to_json(influx_config)
+    if args.db == 'victoria':
+        victoria_url = args.victoria_url
+    else:
+        influx = get_influx_connection(influx_config, test_write=True, test_read=True)
 
     if args.type == "MySQL" or args.type == "MariaDB":
-        # connect to MySQL/MariaDB database
         connection = mysql_connect(host=args.host,
                                    user=args.user,
                                    password=args.password,
@@ -134,32 +181,26 @@ def main():
                                    cursorclass=cursors.SSCursor,
                                    charset="utf8")
     else:
-        # connect to SQLite file instead
         connection = sqlite3.connect(args.database)
 
-    # Create list tables that we are going to retrieve from HA
     tables = get_tables(args.table)
-    print(f"Migrating home assistant database (tables {', '.join(tables)}) to Influx database {args.database} and " +
-          f"bucket {influx_config.get('bucket')}")
-    # write to influxdb in batches
+    print(f"Migrating home assistant database (tables {', '.join(tables)}) to {args.db} database.")
+
     influx_batch_size_max = 1024
     influx_batch_size_cur = 0
-    influx_batch_json = []
+    influx_batch_lines = []
 
     if 'statistics' in tables:
         cursor = connection.cursor()
         tmp_table_query = formulate_tmp_table_sql()
         cursor.execute(tmp_table_query)
-        print(tmp_table_query)
+        #print(tmp_table_query)
         cursor.close()
 
-    # select the values we are interested in
     for table in tables:
         print(f"Running SQL query on database table {table}." +
               "This may take longer than a few minutes, depending on how many rows there are in the database.")
         if args.row_count == 0:
-            # query number of rows in states table - this will be more than the number of rows we
-            # are going to process, but at least it gives us some percentage and estimation
             cursor = connection.cursor()
             cursor.execute(f"select COUNT(*) from {table}")
             total = cursor.fetchone()[0]
@@ -167,20 +208,25 @@ def main():
         else:
             total = args.row_count
 
-        # map to count names and number of measurements for each entity
         statistics = {}
-        # Execute correct query for table
-        sql_query = formulate_sql_query(table, args.table)
+        sql_query = formulate_sql_query(table, args.table, total)
         cursor = connection.cursor()
-        print(sql_query)
+        #print(sql_query)
         cursor.execute(sql_query)
 
-        # Loop over each data row
-        print(f"    Processing max. {total} rows from table {table} and writing to InfluxDB.")
-        with tqdm(total=total, mininterval=1, maxinterval=5, unit=" rows", unit_scale=True, leave=False) as progress_bar:
+        print(
+            f"    Processing max. {total} rows from table {table} and writing to {args.db} database.")
+        with tqdm(total=total, mininterval=1, maxinterval=5, unit=" rows", unit_scale=True,
+                  leave=True) as progress_bar:
             try:
                 row_counter = 0
                 for row in cursor:
+
+                    if row_counter >= total:
+                        print(f"Лимит обработанных строк ({total}) достигнут.")
+                        #break
+
+
                     progress_bar.update(1)
                     row_counter += 1
                     try:
@@ -202,11 +248,10 @@ def main():
                             _event_type = row[5]
                             _time_fired = datetime.fromtimestamp(row[6])
                     except Exception as e:
-                        print("Failed extracting data from %s: %s.\nAttributes: %s" % (row, e, _attributes_raw))
+                        print(f"Failed extracting data from {row}: {e}.\nAttributes: {_attributes_raw}")
                         continue
 
                     try:
-                        # recreate state and event
                         state = State(
                             entity_id=_entity_id,
                             state=_state,
@@ -219,76 +264,63 @@ def main():
                     except InvalidEntityFormatError:
                         pass
                     else:
-                        data = converter(event)
-                        if not data and _state not in ("unavailable", "unknown", "off", "on", "home", "not_home"): # skipping these states is ok
-                            print(f"skipping {_entity_id} with state {_state} at {_time_fired}.")
-                            continue
-                        
-                        if row_counter < 10:
-                            print(f"Example: inserting {_entity_id} with state {_state} at {_time_fired}, attributes={_attributes}.")
+                        line_protocol = convert_to_line_protocol(event)
+                        #metric1 = f"{_entity_id} state={_state},mean={_attributes.get('mean')},min={_attributes.get('min')},max={_attributes.get('max')} {int(_time_fired.timestamp() * 1e9)}"
+                        #metric2 = f'{_entity_id}{{mean="{_mean}",min="{_min}",max="{_max}"}} {int(_time_fired.timestamp())}'
+                        #metric3 = f"{_entity_id} state={_state} mean={_mean} min={_min} max={_max} {int(_time_fired.timestamp())}000000000"
 
-                        # collect statistics (remove this code block to speed up processing slightly)
-                        if "friendly_name" in _attributes:
-                            friendly_name = _attributes["friendly_name"]
-
-                            if _entity_id not in statistics:
-                                statistics[_entity_id] = {friendly_name: 1}
-                            elif friendly_name not in statistics[_entity_id]:
-                                statistics[_entity_id][friendly_name] = 1
-                                print("Found new name '%s' for entity '%s'. All names known so far: %s" % (
-                                        friendly_name, _entity_id, statistics[_entity_id].keys()))
-                                print(row)
-                            else:
-                                statistics[_entity_id][friendly_name] += 1
-
-                        influx_batch_json.append(data)
-                        influx_batch_size_cur += 1
+                        if _state not in (
+                                "unavailable", "unknown", "off", "on", "home",
+                                "not_home") \
+                                and _entity_id not in ('plant.pepper'): # skipping these states is ok
+                            influx_batch_lines.append(line_protocol)
+                            influx_batch_size_cur += 1
+                        #  else:
+                        #    print(f"skipping {_entity_id} with state {_state} at {_time_fired}.")
 
                         if influx_batch_size_cur >= influx_batch_size_max:
                             if not args.dry:
-                                influx.write(influx_batch_json)
+                                if args.db == 'victoria':
+                                    requests.post(f"{victoria_url}/write", data = "\n".join(influx_batch_lines).encode('utf-8'))
+                                else:
+                                        influx.write("\n".join(influx_batch_lines))
                             influx_batch_size_cur = 0
-                            influx_batch_json = []
+                            influx_batch_lines = []
+
             except Error as mysql_error:
                 print(f"MySQL error on row {row_counter}: {mysql_error}")
                 continue
-            progress_bar.close()
-            cursor.close()
+            finally:
+                progress_bar.close()
+                cursor.close()
 
-    if not args.dry:
-        influx.write(influx_batch_json)
-    # Clean up by closing influx connection, and removing temporary table
-    influx.close()
-    if args.table == 'both':
-        remove_tmp_table(cursor)
+    if not args.dry and influx_batch_lines:
+        if args.db == 'victoria':
+            requests.post(f"{victoria_url}/write", data="\n".join(influx_batch_lines).encode('utf-8'))
+        else:
+            influx.write("\n".join(influx_batch_lines))
 
-    # print statistics - ideally you have one friendly name per entity_id
-    # you can use the output to see where the same sensor has had different
-    # names, as well as which entities do not have lots of measurements and
-    # thus could be ignored (add them to exclude/entities in the influxdb yaml)
-    for entity in sorted(statistics.keys()):
-        print(entity)
-        for friendly_name in sorted(statistics[entity].keys()):
-            count = statistics[entity][friendly_name]
-            print("  - %s (%d)" % (friendly_name, count))
+    print("Finished writing data to the database, closing connections.")
+    connection.close()
 
 
-def get_tables(table_key: str) -> list:
+def formulate_tmp_table_sql():
     """
-    Switch case statement for tables
+    Create temporary LUT to map attributes_id to entity_id to improve performance.
+
+    TODO Not perfect solution, some entities have multiple attributes that change over time.
+    TODO Here we select the most recent
     """
-    switcher = {
-            'states': ['states'],
-            'statistics': ['statistics'],
-            'both': ['statistics', 'states']
-    }
-    try:
-        return switcher[table_key]
-    except KeyError:
-        print("ERROR: argument --table should be \"states\" or \"statistics\"")
+    return """CREATE TABLE IF NOT EXISTS state_tmp AS
+    SELECT max(states.attributes_id) as attributes_id, states_meta.entity_id
+    FROM states, states_meta
+    WHERE states.metadata_id = states_meta.metadata_id
+      AND states.attributes_id IS NOT NULL
+    GROUP BY states_meta.entity_id;
+    """
 
 
-def formulate_sql_query(table: str, arg_tables: str):
+def formulate_sql_query(table: str, arg_tables: str, total: int):
     """
     Retrieves data from the HA databse
     """
@@ -304,15 +336,18 @@ def formulate_sql_query(table: str, arg_tables: str):
                        from states, state_attributes, states_meta
                        where event_id is null
                         and states.attributes_id = state_attributes.attributes_id
-                        and states.metadata_id = states_meta.metadata_id;
+                        and states.metadata_id = states_meta.metadata_id
                         """
+        sql_query += f" LIMIT {total};"
+
     elif table == "statistics":
         if arg_tables == 'both':
             # If we're adding both, we should not add statistics for the same time period we're adding events
             inset_query = f"{sql_query}" + \
-                f"\n         AND statistics.start < (select min(events.time_fired) as datetetime_start from events)"
+                f"\n         AND statistics.start_ts < (select min(events.time_fired_ts) as datetetime_start from events)"
         else:
-            inset_query = "\n         AND statistics.start_ts < 1708837680"
+            # inset_query = "\n         AND statistics.start_ts < 1708837680"
+            inset_query = "\n         AND statistics.start_ts > 1630894088"
             # start 25.2. 6:10 MEZ = 1708837680
         sql_query = f"""
         SELECT statistics_meta.statistic_id,
@@ -336,27 +371,11 @@ def formulate_sql_query(table: str, arg_tables: str):
     return sql_query
 
 
-def formulate_tmp_table_sql():
-    """
-    Create temporary LUT to map attributes_id to entity_id to improve performance.
-
-    TODO Not perfect solution, some entities have multiple attributes that change over time.
-    TODO Here we select the most recent
-    """
-    return """CREATE TABLE IF NOT EXISTS state_tmp AS
-    SELECT max(states.attributes_id) as attributes_id, states_meta.entity_id
-    FROM states, states_meta
-    WHERE states.metadata_id = states_meta.metadata_id
-      AND states.attributes_id IS NOT NULL
-    GROUP BY states_meta.entity_id;
-    """
-
-
-def remove_tmp_table(cursor):
-    try:
-        cursor.execute("""DROP TABLE state_tmp;""")
-    except Exception as e:
-        print("Error during drop", e)
+def get_tables(table_type: str) -> list:
+    if table_type == "both":
+        return ["states", "statistics"]
+    else:
+        return [table_type]
 
 
 if __name__ == "__main__":
